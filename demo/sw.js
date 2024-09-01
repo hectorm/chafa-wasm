541c8aca504975931cefe241cde6c5991959826b
Héctor Molinero Fernández <hector@molinero.dev>
2024-09-01 15:13:41 +0200

    Refactor cache logic

--- demo/sw.js
+++ demo/sw.js
@@ -3,18 +3,18 @@
 (() => {
   const self = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (globalThis.self));
 
-  const cacheVersion = 2;
-  const currentCaches = { main: `main-cache-v${cacheVersion}` };
+  const cacheVersion = 1;
+  const currentCaches = { app: `app-cache-v${cacheVersion}` };
 
   self.addEventListener("activate", (event) => {
     event.waitUntil(
       (async () => {
-        const cacheNames = await caches.keys();
+        const cacheNames = await globalThis.caches.keys();
         const expectedCacheNames = Object.values(currentCaches);
         return Promise.all(
           cacheNames.map((cacheName) => {
             if (expectedCacheNames.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
+              return globalThis.caches.delete(cacheName);
             }
           }),
         );
@@ -23,26 +23,22 @@
   });
 
   self.addEventListener("fetch", (event) => {
-    if (
-      !self.location.origin.startsWith("https://") ||
-      !event.request.url.startsWith(self.location.origin) ||
-      !event.request.url.startsWith("https://cdnjs.cloudflare.com/") ||
-      event.request.method !== "GET"
-    ) {
-      return;
+    const methods = ["GET", "HEAD"];
+    const prefixes = [self.location.origin, "https://cdnjs.cloudflare.com/"];
+    if (methods.some((s) => event.request.method === s) && prefixes.some((s) => event.request.url.startsWith(s))) {
+      event.respondWith(
+        (async () => {
+          const cache = await globalThis.caches.open(currentCaches.app);
+          const cacheResponse = await cache.match(event.request);
+          const fetchPromise = fetch(event.request).then((fetchResponse) => {
+            if (fetchResponse.status >= 200 && fetchResponse.status <= 299) {
+              cache.put(event.request, fetchResponse.clone());
+            }
+            return fetchResponse;
+          });
+          return cacheResponse ?? fetchPromise;
+        })(),
+      );
     }
-    event.respondWith(
-      (async () => {
-        const cache = await caches.open(currentCaches.main);
-        const cacheResponse = await cache.match(event.request);
-        const fetchPromise = fetch(event.request).then((fetchResponse) => {
-          if (fetchResponse.status >= 200 && fetchResponse.status <= 299) {
-            cache.put(event.request, fetchResponse.clone());
-          }
-          return fetchResponse;
-        });
-        return cacheResponse ?? fetchPromise;
-      })(),
-    );
   });
 })();
